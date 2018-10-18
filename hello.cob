1f2377485be034bf7ef69491582226c70ee3ff76
Igor Matheus Diniz <igor.pinheiro@ccc.ufcg.edu.br>
2018-10-18 13:55:27 -0300

    Add Hello World in Cobol language

---
+++ hello.cob
@@ -0,0 +1,11 @@
+* Hello World Program
+* Igor Matheus Diniz
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.    hello.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+
+       PROCEDURE DIVISION.
+           DISPLAY "hello ," WITH NO ADVANCING 
+           DISPLAY "world!"
+           STOP RUN.
